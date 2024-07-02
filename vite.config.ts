352ea716a0f21fcf41e4658bc54420f8ec6bd1d6
Jutipong <techcom.ec20@gmail.com>
2024-07-02 16:40:52 +0700

    chore: Update VITE_PORT to 4000 in .env file and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   envDir: '.env',
+//   plugins: [vue()],
+// })
+
+
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue()],
+
+    server: {
+      port: parseInt(process.env.VITE_PORT ?? '3000'),
+    },
+  });
 })
